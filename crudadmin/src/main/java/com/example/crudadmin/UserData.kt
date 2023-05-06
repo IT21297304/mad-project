2a16a6401e7ab1b3b4eee8746832574fa1fae468
IT2345746 <IT21345746@my.sliit.lk>
2023-05-07 00:13:01 +0530

    Files added

---
+++ crudadmin/src/main/java/com/example/crudadmin/UserData.kt
@@ -0,0 +1,5 @@
+package com.example.crudadmin
+
+data class UserData(val name : String? = null, val phone : String? = null){
+
+}
---
+++ cruditem/src/main/java/com/example/cruditem/UserData.kt
@@ -0,0 +1,5 @@
+package com.example.cruditem
+
+data class UserData(val name : String? = null, val phone : String? = null){
+
+}
